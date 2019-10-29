a0e1b305bd8f7e4846c109752f695d6bb4b7b84f
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2019-10-29 20:02:45 +0100

    stats update

--- cemetery-app/src/app/pie-chart/pie-chart.component.ts
+++ cemetery-app/src/app/pie-chart/pie-chart.component.ts
@@ -30,7 +30,7 @@ import { DataService } from '../data.service';
 export class PieChartComponent implements OnInit {
   
   public pieChartLabels = ['Kobiety', 'Mężczyzni'];
-  public pieChartData = [137, 202];
+  public pieChartData = [138, 203];
   public pieChartType = 'pie';
 
   
