d7344457af9a7cc4bad0f17fcf5306367e845eb7
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2022-12-12 21:25:49 +0100

    [gallery] update gallery component, add photos for year seasons

--- cemetery-app/src/app/gallery/gallery.component.ts
+++ cemetery-app/src/app/gallery/gallery.component.ts
@@ -7,20 +7,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  public imageObject = [
-    {
-      image: "assets/img/cem1.jpg",
-      thumbImage: "assets/img/cem1.jpg"
-    },
-    {
-      image: "assets/img/cem2.jpg",
-      thumbImage: "assets/img/cem2.jpg"
-    },
-    {
-      image: "assets/img/cem3.jpg",
-      thumbImage: "assets/img/cem3.jpg"
-    }
-  ];
+  public images = {
+    spring: [
+      {
+        image: "assets/img/gallery/spring/cem1.jpg",
+        thumbImage: "assets/img/gallery/spring/cem1.jpg"
+      },
+      {
+        image: "assets/img/gallery/spring/cem2.jpg",
+        thumbImage: "assets/img/gallery/spring/cem2.jpg"
+      },
+      {
+        image: "assets/img/gallery/spring/cem3.jpg",
+        thumbImage: "assets/img/gallery/spring/cem3.jpg"
+      }
+    ],
+    summer: [
+      {
+        image: "assets/img/gallery/summer/summer-11.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-11.JPG"
+      },
+      {
+        image: "assets/img/gallery/summer/summer-22.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-22.JPG"
+      },
+      {
+        image: "assets/img/gallery/summer/summer-33.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-33.JPG"
+      },
+      {
+        image: "assets/img/gallery/summer/summer-44.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-44.JPG"
+      },
+      {
+        image: "assets/img/gallery/summer/summer-55.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-55.JPG"
+      },
+      {
+        image: "assets/img/gallery/summer/summer-66.JPG",
+        thumbImage: "assets/img/gallery/summer/summer-66.JPG"
+      },
+    ],
+    autumn: [],
+    winter: [
+      {
+        image: "assets/img/gallery/winter/winter-1.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-1.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-2.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-2.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-3.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-3.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-4.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-4.jpg"
+      }, 
+      {
+        image: "assets/img/gallery/winter/winter-5.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-5.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-6.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-6.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-7.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-7.jpg"
+      },
+      {
+        image: "assets/img/gallery/winter/winter-8.jpg",
+        thumbImage: "assets/img/gallery/winter/winter-8.jpg"
+      },
+    ]
+  }
 
   constructor() { }
 
