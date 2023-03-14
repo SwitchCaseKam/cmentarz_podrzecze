9da09042da440b242e645138ad1d83888e8a6867
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2023-03-14 21:48:15 +0100

    add event listener to map

--- cemetery-app/src/app/map-locator/map/map.component.ts
+++ cemetery-app/src/app/map-locator/map/map.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TombMarkerService } from 'src/app/services/tomb-marker.service';
 
@@ -8,22 +8,28 @@ import { TombMarkerService } from 'src/app/services/tomb-marker.service';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
   public x = '-10%';
   public y = '-10%';
 
   constructor(
     public dialog: MatDialog,
-    private tombMarkerService: TombMarkerService
+    private tombMarkerService: TombMarkerService,
+    private renderer2: Renderer2
   ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.tombMarkerService.getXValue().subscribe(xVal => this.x = xVal);
     this.tombMarkerService.getYValue().subscribe(yVal => this.y = yVal);
   }
 
-  public getTombInfo(event: Event) {
-    this.tombMarkerService.redirectToMapAndMarkTomb(parseInt(event.target['id']));
+  public ngAfterViewInit(): void {
+    const tombs = Array.from(document.getElementsByClassName('tomb'));
+    tombs.forEach(tomb => this.renderer2.listen(tomb, 'click', () => this.getTombInfo(tomb)));
+  }
+
+  public getTombInfo(element: Element): void {
+    this.tombMarkerService.redirectToMapAndMarkTomb(parseInt(element.id));
   }
 }
